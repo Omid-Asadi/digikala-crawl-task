cf9300a40d0ba1a3bc8b0283b3de8fd2deba0943
omid <omidasadi.70@gmail.com>
2022-12-12 04:48:26 +0330

    Done

--- CrawlerServices/configure/constants.py
+++ CrawlerServices/configure/constants.py
@@ -1,6 +1,6 @@
 START_PAGE = 0
 BULK_LINK_BATCH_SIZE = 3
-
+RUNNER = True
 TARGET_URL = "https://api.torob.com/v4/base-product/search/?category=243&sort=popularity&page={}&size={}&source=next_" \
              "desktop&android_app_ver=None&_bt__experiment=&suid=63961e5ab5a0ded987cb93a8"
 STOP_FLAG = 'next'
