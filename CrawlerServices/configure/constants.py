cbf332ca2b9a1ab635d623b27d76f78d19e442ea
omid <omidasadi.70@gmail.com>
2022-12-12 03:54:22 +0330

    docker branch added

--- CrawlerServices/configure/constants.py
+++ CrawlerServices/configure/constants.py
@@ -1,5 +1,8 @@
-START_PAGE = 0
-BULK_LINK_BATCH_SIZE = 3
+import os
+
+
+START_PAGE = os.environ.get('START_PAGE')
+BULK_LINK_BATCH_SIZE = os.environ.get('BULK_LINK_BATCH_SIZE')
 
 TARGET_URL = "https://api.torob.com/v4/base-product/search/?category=243&sort=popularity&page={}&size={}&source=next_" \
              "desktop&android_app_ver=None&_bt__experiment=&suid=63961e5ab5a0ded987cb93a8"
