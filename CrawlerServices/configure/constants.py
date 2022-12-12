33098881667834794c906c1fdb3e3350bd7778f9
omid <omidasadi.70@gmail.com>
2022-12-12 04:52:28 +0330

    Done

--- CrawlerServices/configure/constants.py
+++ CrawlerServices/configure/constants.py
@@ -3,6 +3,7 @@ import os
 
 START_PAGE = os.environ.get('START_PAGE')
 BULK_LINK_BATCH_SIZE = os.environ.get('BULK_LINK_BATCH_SIZE')
+RUNNER = os.environ.get('RUNNER')
 
 TARGET_URL = "https://api.torob.com/v4/base-product/search/?category=243&sort=popularity&page={}&size={}&source=next_" \
              "desktop&android_app_ver=None&_bt__experiment=&suid=63961e5ab5a0ded987cb93a8"
