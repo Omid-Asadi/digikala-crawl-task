1f747e5be022784a977d2d52f384c8eef94c323e
omid <omidasadi.70@gmail.com>
2022-12-16 11:04:09 +0330

    folders changed to run independently

--- CrawlerServices/src/runner.py
+++ CrawlerServices/src/runner.py
@@ -1,8 +1,8 @@
 import os
-
 from configure.constants import RUNNER
 from configure.logger import process_log
 from crawl.crawl import crawl
+
 
 if RUNNER:
     process_log.info(f"['crawling started!']")
